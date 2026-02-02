e0de677a503df19279ed5335b4e7a58f37ffe621
Varun <vpahuja1508@gmail.com>
2026-02-02 14:29:06 +0530

    Pushing Till Lab 3, i.e 2/2/26

---
+++ test.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Value of a: "))
+b = int(input("Enter Value of b "))
+a,b = b,a
+print("After Swapping : ")
+print("This is a: ",end="")
+print(a)
+print("This is b: ",end="")
+print(b)
